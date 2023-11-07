15eb788597f9917a5968f1eb9cdf5e258eee3d77
itc-n23019 <131750031+itc-n23019@users.noreply.github.com>
2023-11-07 16:44:57 +0900

    Create addserver.py

---
+++ addserver.py
@@ -0,0 +1,47 @@
+import socket
+import struct
+
+def send_msg(sock, msg):
+    total_sent_len = 0
+    total_msg_len = len(msg)
+    while total_sent_len < total_msg_len:
+        sent_len = sock.send(msg[total_sent_len:])
+        if sent_len == 0:
+            raise RuntimeError('socket connection broken')
+        total_sent_len += sent_len
+
+
+def recv_msg(sock, total_msg_size):
+    total_recv_size = 0
+    while total_recv_size < total_msg_size:
+        received_chunk = sock.recv(total_msg_size - total_recv_size)
+        if len(received_chunk) == 0:
+            raise RuntimeError('socket connection broken')
+        yield received_chunk
+        total_recv_size += len(received_chunk)
+
+def main():
+    ip_address = input("Enter the IP address: ")
+    port = int(input("Enter the port number: "))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    server_socket.bind((ip_address, port))
+    server_socket.listen()
+    print('starting server ...')
+    client_socket, (client_address, client_port) = server_socket.accept()
+    print(f'accepted from {client_address}:{client_port}')
+    received_msg = b''.join(recv_msg(client_socket, total_msg_size=8))
+    print(f'received: {received_msg}')
+    (ope1, ope2) = struct.unpack('!ii', received_msg)
+    print(f'operand1: {ope1}, operand2: {ope2}')
+    result = ope1 + ope2
+    print(f'result: {result}')
+    result_msg = struct.pack('!q', result)
+    send_msg(client_socket, result_msg)
+    print(f'sent: {result_msg}')
+    client_socket.close()
+    server_socket.close()
+:
+if __name__ == '__main__':
+    main()
+
