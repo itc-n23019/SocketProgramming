f5d215c45315f88450d0d0f4399e485777335758
itc-n23019 <131750031+itc-n23019@users.noreply.github.com>
2023-11-07 14:28:46 +0900

    Update 8_1_2.py

--- 8_1_2.py
+++ 8_1_2.py
@@ -21,7 +21,7 @@ def main():
     port = int(input("Enter the port number: "))
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Corrected the typos
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
     server_socket.bind((ip_address, port))
     server_socket.listen()
     print("Starting server ...")
